47adaae0e52e1877a672efce1fded34721a5811d
Xianquan (Sarinstein) Yan <98195226+sarinstein-yan@users.noreply.github.com>
2025-01-07 20:29:40 +0800

    Update __init__.py

--- src/sg_stat/__init__.py
+++ src/sg_stat/__init__.py
@@ -1,6 +1,12 @@
 __version__ = "0.0.0"
 
 from .perturb import *
+from .multiproc import main_workflow_mpc
 from .plot import *
 
-__all__ = ["main_workflow", "plot_properties_vs_A_separate", "plot_correlations_vs_A_separate"]
+__all__ = [
+"main_workflow",
+"main_workflow_mpc",
+"plot_properties_vs_A_separate",
+"plot_correlations_vs_A_separate"
+]
